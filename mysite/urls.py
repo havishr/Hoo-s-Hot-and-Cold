003f27480da5fe9387db99903d822eb99fb7e666
Vale Lovan <vrl5chk@virginia.edu>
2023-10-01 14:42:45 -0400

    Set up Google login.

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic import TemplateView
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("accounts/", include("allauth.urls")),
+    path("", include("oauth_app.urls"))
 ]

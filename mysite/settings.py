33e6df7ea56e3e43cfc53a81e1326b2e74fc2d89
Vale Lovan <vrl5chk@virginia.edu>
2023-11-02 16:32:46 -0400

    Added Heroku check to fix constantly changing site id issue.

--- mysite/settings.py
+++ mysite/settings.py
@@ -40,6 +40,9 @@ ALLOWED_HOSTS = ['adv-software-dev-project-82c26c41941d.herokuapp.com', 'localho
 # Application definition
 # From https://www.youtube.com/watch?v=yO6PP0vEOMc
 SITE_ID = 6
+ON_HEROKU = os.environ.get('ON_HEROKU')
+if ON_HEROKU:
+    SITE_ID = 5
 
 INSTALLED_APPS = [
     # From https://github.com/heroku/python-getting-started/blob/main/gettingstarted/settings.py

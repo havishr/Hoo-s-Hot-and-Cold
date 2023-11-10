74541b96e1e9d13828007472cb36d1e13fe58357
Vale Lovan <vrl5chk@virginia.edu>
2023-11-10 14:13:04 -0500

    Testing different Django version.

--- mysite/settings.py
+++ mysite/settings.py
@@ -127,7 +127,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 #     }
 
 ON_HEROKU = os.environ.get('ON_HEROKU')
-# ON_HEROKU = True
+
 
 if ON_HEROKU:
     DATABASES = {
@@ -135,12 +135,16 @@ if ON_HEROKU:
     }
     # print(os.environ.get('DATABASE_URL'))
 else:
+    # DATABASES = {
+    #     'default': {
+    #         'ENGINE': 'django.db.backends.sqlite3',
+    #         'NAME': BASE_DIR / 'db.sqlite3',
+    #     }
+    # }
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
+        'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
     }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators

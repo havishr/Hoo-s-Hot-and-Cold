67444f1d3806368bac639247cd150286ee3882e3
Vale Lovan <vrl5chk@virginia.edu>
2023-11-10 14:18:29 -0500

    Changed db to not point to Postgres locally.

--- mysite/settings.py
+++ mysite/settings.py
@@ -135,15 +135,15 @@ if ON_HEROKU:
     }
     # print(os.environ.get('DATABASE_URL'))
 else:
-    # DATABASES = {
-    #     'default': {
-    #         'ENGINE': 'django.db.backends.sqlite3',
-    #         'NAME': BASE_DIR / 'db.sqlite3',
-    #     }
-    # }
     DATABASES = {
-        'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
+    # DATABASES = {
+    #     'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+    # }
 
 
 # Password validation
